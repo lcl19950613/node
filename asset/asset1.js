b21bde0a3aba05aeaf0a41290a60fe825a928612
Anikinly <qyl_tjshydx@qq.com>
2016-06-04 17:43:58 +0800

    assert 暂时跳过单元测试

---
+++ asset/asset1.js
@@ -0,0 +1,32 @@
+/**
+ * Created by Anikin on 2016/6/4.
+ */
+
+// 这个模块一般用于单元测试,断言测试,暂时跳过单元测试章节，需要在一定的编程基础之上再学习
+
+var assert = require('assert');
+    //console.log( assert );
+
+/*
+  assert.fail(actual, expected, message, operator)
+  使用参数 operator 测试参数 actual (实际值) 和 expected （期望值）是否相等。
+ */
+
+/*
+* assert(value[, message]), assert.ok(value[, message])
+* 测试参数 value 是否为 true ,此函数和 assert.equal(true, !!value, message); 等价
+* */
+
+ var show = function(a,b){
+     return a+b;
+ };
+
+/*
+* assert.equal( actual,expected,[message] )
+*/
+
+
+
+
+
+
