20bdc5ed7d7a4d37171e30453cf1c2ed55f6041e
仇彦龙 <qyl_tjshydx@qq.com>
2016-12-15 18:50:26 +0800

    copyfile.js

---
+++ stream/copyfile.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Administrator on 2016/12/15 0015.
+ */
+    var fs = require("fs");
+
+    // 创建一个可读流
+    var readerStream = fs.createReadStream('output.txt');
+
+    // 创建一个可写流
+    var writerStream = fs.createWriteStream('copyput.txt');
+
+    // 管道读写操作
+    // 读取 input.txt 文件内容，并将内容写入到 output.txt 文件中
+    readerStream.pipe(writerStream);
+
+    console.log("程序执行完毕");
